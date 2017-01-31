9d4cc0362e2701593f0611328ec7762057d6b95d
EC2 Default User <ec2-user@ip-172-31-50-252.ec2.internal>
2017-01-31 21:46:11 +0000

    most recent version, still having issues with setting device memory propery/copying it back to host to check for bit flips, as well as some odd pointer arithmetic

--- cudahammer_test.cu
+++ cudahammer_test.cu
@@ -5,12 +5,12 @@
 __global__ void kernel(int** addrs, int addr_count, int toggles) {
 	// for (int i = 0; i < toggles; i++) {
 		for (int a = 0; a < addr_count; a++) {
-			asm("ld.global.cv rd1, [%0];" : "=l"(addrs[a]));
+			asm("ld.param.cv.u32 %%rd1, [%0];" : "=l"(addrs[a]));
 		}
 	//}
 }
 
-void hammer_attempt(int toggles, int addr_count, int mem_size, void* mem) {
+void hammer_attempt(int toggles, int addr_count, const size_t mem_size, int* mem) {
 
 	printf("entered hammer_attempt\n");
 	// declare timer
@@ -28,8 +28,7 @@ void hammer_attempt(int toggles, int addr_count, int mem_size, void* mem) {
 	timer.Start();
 	for (int i = 0; i < toggles; i++) {
 		printf("toggle %d\n", i);
-		kernel<<<1,1>>>(addrs, addr_count, toggles);
-	
+		kernel<<<1,1>>>(addrs, addr_count, toggles);	
 	}
 	timer.Stop();
 
@@ -37,43 +36,81 @@ void hammer_attempt(int toggles, int addr_count, int mem_size, void* mem) {
 	printf("%d memory accesses done on %d addresses in %g ms\n", toggles, addr_count, timer.Elapsed());
 
 	// check for errors
+	// copy device memory to host first
 	printf("checking for bit flips...\n");
+	printf("mem size sanity check: %d\n", mem_size);
+	int* hostmem = (int*) malloc(mem_size);
+	printf("hostmem address is %p\n", (void*) hostmem);
+	printf("past host malloc\n");
+	memset(hostmem, 0, mem_size);
+	printf("past host memset\n");
+	cudaError_t memcpyerr = cudaMemcpy(hostmem, mem, mem_size, cudaMemcpyDeviceToHost);	
+	if (memcpyerr == cudaErrorInvalidValue) {
+		printf("cudaMemcpy failed cudaErrorInvalidValue\n");		
+		exit(-1);
+	}
+	if (memcpyerr == cudaErrorInvalidDevicePointer) {
+		printf("cudaMemcpy failed cudaErrorInvalidDevicePointer\n");		
+		exit(-1);
+	}
+	if (memcpyerr == cudaErrorInvalidMemcpyDirection) {
+		printf("cudaMemcpy failed cudaErrorMemcpyDirection\n");		
+		exit(-1);
+	}
+	printf("past the cudaMemcpyDeviceToHost\n");
 	int flips = 0;
 	int* pointer;
-	int* mem_end = ((int*) mem + mem_size);
-	for (pointer = (int*) mem; pointer < mem_end; pointer++) {
+	int* mem_end = (int*) hostmem + mem_size;
+	printf("mem_end address is %p\n", (void*) mem_end);
+	for (pointer = hostmem; pointer < mem_end; pointer++) {
 		int val = *pointer;
+		//printf("past val\n");
+		/*
 		if (val != (int) 0xff) {
-			printf("found error!\n");
+			flips++;
+			//printf("found error!\n");
 		}
+		*/
+		printf("%d\n", val);
 	}
 	printf("found %d errors\n", flips);
 }
 
 
 int main(int argc, char** argv) {
-	
-	// the amount of memory (in GB) to allocate for picking addresses from
-	int num_gb = atoi(argv[1]); 
-	int mem_size = (2 << 30) * num_gb;
+	 
+	// amount of memory to allocate (1GB)
+	const size_t mem_size = (1 << 30);
 	
 	// for a particular set of chosen addresses, the number of times to access each one
-	int toggles = atoi(argv[2]);
+	int toggles = atoi(argv[1]);
 
 	// the number of addresses to be chosen for a hammering attempt
 	// google test chooses 8, the original approach just uses 2 (has to be at least 2)
-	int addr_count = atoi(argv[3]);
+	int addr_count = atoi(argv[2]);
 
 	// the number of times to choose a set of addresses
 	// passing 0 makes the program continue to choose sets of addresses until stopped or until an error is found
-	int iterations = atoi(argv[4]);
+	int iterations = atoi(argv[3]);
 
 	// allocate a mem_size chunk of memory on the device
 	int* mem; 
-	cudaMalloc((void**)&mem, mem_size);
-
+	cudaError_t mallocerr = cudaMalloc((void**)&mem, mem_size);
+	if (mallocerr == cudaErrorMemoryAllocation) {
+		printf("cudaMalloc failed, exiting\n");
+		exit(-1);
+	}
 	// set all bits to 1
-	cudaMemset(&mem, 0xff, (size_t) mem_size);
+	cudaError_t memseterr = cudaMemset(mem, 0xff, mem_size);
+	if (memseterr == cudaErrorInvalidValue) {
+		printf("cudaMemset failed cudaErrorInvalidValue\n");
+		exit(-1);
+	}
+	if (memseterr == cudaErrorInvalidDevicePointer) {
+		printf("cudaMemset failed cudaErrorInvalidDevicePointer\n");
+		exit(-1);
+	}
+	
 
 	// as mentioned above, either run iterations number of hammer attempts, or run until stopped
 	if (iterations > 0) {
